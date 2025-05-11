a563ca1203546f2d0573007650a5f3d7b2609438
erenzdn <mehmeteren850@gmail.com>
2025-05-11 22:15:45 +0300

    İlk commit

---
+++ src/test/java/com/example/EarthquakeCheck/EarthquakeCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EarthquakeCheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarthquakeCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
